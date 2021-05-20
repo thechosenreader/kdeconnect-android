bcf7fee983419346daf3adaefb5d9a51a54af442
thechosenreader <mmotiwala53@gmail.com>
2021-05-19 22:47:53 -0400

    created randomstring function for characters only

--- src/org/kde/kdeconnect/Helpers/RandomHelper.java
+++ src/org/kde/kdeconnect/Helpers/RandomHelper.java
@@ -6,15 +6,27 @@ import java.security.SecureRandom;
 public class RandomHelper {
     public static final SecureRandom secureRandom = new SecureRandom();
 
-    private static final char[] symbols = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-            "abcdefghijklmnopqrstuvwxyz" +
-            "1234567890").toCharArray();
+    private static final char[] chars   = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                                           "abcdefghijklmnopqrstuvwxyz")
+                                           .toCharArray();
+
+    private static final char[] symbols = (new String(chars) +
+                                          "0123456789")
+                                          .toCharArray();
 
 
     public static String randomString(int length) {
         char[] buffer = new char[length];
         for (int idx = 0; idx < length; ++idx) {
             buffer[idx] = symbols[secureRandom.nextInt(symbols.length)];
+        }
+        return new String(buffer);
+    }
+
+    public static String randomStringCharOnly(int length) {
+        char[] buffer = new char[length];
+        for (int idx = 0; idx < length; ++idx) {
+            buffer[idx] = chars[secureRandom.nextInt(chars.length)];
         }
         return new String(buffer);
     }
